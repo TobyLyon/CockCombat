49bee739a0e821154088164ee2464b521a3e4b15
TobyLyon <toby.hmg@gmail.com>
2025-10-09 00:40:53 -0700

    chore(HUD): update control panel to Space=Jump, Shift=Peck

--- components/battle/control-panel.tsx
+++ components/battle/control-panel.tsx
@@ -14,8 +14,8 @@ const ControlPanel: React.FC<ControlPanelProps> = (props) => {
       </div>
       <div className="space-y-1 sm:space-y-2">
         <p><span className="font-semibold text-orange-400 w-12 sm:w-16 inline-block text-xs">WASD:</span> <span className="text-xs">Move</span></p>
-        <p><span className="font-semibold text-orange-400 w-12 sm:w-16 inline-block text-xs">Space:</span> <span className="text-xs">Attack</span></p>
-        <p><span className="font-semibold text-orange-400 w-12 sm:w-16 inline-block text-xs">Shift:</span> <span className="text-xs">Jump</span></p>
+        <p><span className="font-semibold text-orange-400 w-12 sm:w-16 inline-block text-xs">Space:</span> <span className="text-xs">Jump</span></p>
+        <p><span className="font-semibold text-orange-400 w-12 sm:w-16 inline-block text-xs">Shift:</span> <span className="text-xs">Peck/Attack</span></p>
       </div>
     </div>
   );
